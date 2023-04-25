5853f1cfaf2ef0bc182e438520018124737b59b0
mukaddam5 <130515762+mukaddam5@users.noreply.github.com>
2023-04-25 01:24:59 -0500

    jj

---
+++ Homework2.py
@@ -0,0 +1,78 @@
+
+
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+Sum = sum(numbers)
+print(Sum)
+
+
+print(0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9)
+
+
+
+def addition(q, w, e, r, t, y, u, i, o, p):
+    return q + w + e + r + t + y + u + i + o + p
+print(addition(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+
+
+q = 0
+w = 1
+e = 2
+r = 3
+t = 4
+y = 5
+u = 6
+i = 7
+o = 8
+p = 9
+print(q + w + e + r + t + y + u + i + o + p)
+
+
+
+def homework(*numbers):
+    total = 0
+    for number in numbers:
+        total = total + number
+    return total
+
+print(homework(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+
+
+
+#
+#
+# num = int(input("Enter a number"))
+# if num < 10:
+#     raise Exception("Provided number cannot be accepted")
+
+
+
+
+# num = int(input("Enter a number"))
+# num1 = int(input("Enter a number"))
+# if num < 10 and num1 > 99:
+#     raise Exception( " Wrong number")
+
+
+
+
+# num = int(input("Enter a number"))
+# if num < 10 or num > 99:
+#     raise Exception("Not accepted")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
